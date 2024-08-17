441e88b7c7e9c18c1ce1727081860c1d4a90ffd1
Atahan <atahanylmaz@gmail.com>
2024-08-17 15:10:01 +0300

    input

--- Assets/Features/Slime/SlimeControllerSystem.cs
+++ Assets/Features/Slime/SlimeControllerSystem.cs
@@ -1,4 +1,5 @@
-﻿using DefaultNamespace;
+﻿using System.Collections;
+using DefaultNamespace;
 using NaughtyAttributes;
 using ScriptableObjectArchitecture;
 using UnityEngine;
@@ -6,8 +7,18 @@ using UnityEngine;
 namespace Features.Slime
 {
     [CreateAssetMenu(menuName = "SlimeControllerSystem")]
-    public class SlimeControllerSystem: ScriptableObject, IInit, IExecute, IFixedExecute
+    public class SlimeControllerSystem: ScriptableObject, IInit, ICleanUp
     {
+        [HorizontalLine(2, EColor.Green)]
+        public GameObject SlimePrefab;
+        public float MaxScale;
+        public float MinScale;
+
+        [HorizontalLine(2, EColor.Blue)]
+        public float MaxMotorForce;
+        public float TargetMotorVelocity;
+        public float MotorForceLerpAmount = 0.1f;
+        
         [HorizontalLine(2, EColor.Blue)]
         public BoolVariable SlimeGrow;
         public BoolVariable SlimeShrink;
@@ -16,22 +27,57 @@ namespace Features.Slime
         public BoolVariable AddForce;
         
         
+        private SlimeMono _slimeMono;
+        
         public void Init()
         {
-            SlimeGrow.AddListener(OnSlimeGrow);
+            _slimeMono = Instantiate(SlimePrefab).GetComponent<SlimeMono>();
+            _slimeMono.SetJointMotor(0,0);
+            GameRoot.CoroutineRunner.StartCoroutine(SlimeControlRoutine());
         }
 
-        private void OnSlimeGrow(bool arg0)
+        private IEnumerator SlimeControlRoutine()
         {
+            while (true)
+            {
+                if(SlimeGrow.Value)
+                {
+                
+                }
+                else if(SlimeShrink.Value)
+                {
+                    _slimeMono.SetSlimeScale(_slimeMono.transform.localScale - Vector3.one * Time.deltaTime);
+                }
             
+                if(HammerGrow.Value)
+                {
+                    _slimeMono.transform.localScale += Vector3.one * Time.deltaTime;
+                }
+                else if(HammerShrink.Value)
+                {
+                    _slimeMono.transform.localScale -= Vector3.one * Time.deltaTime;
+                }
+            
+                if(AddForce.Value)
+                {
+                    var hinge = _slimeMono.Joint;
+                    var lerp = Mathf.Lerp(hinge.motor.force, MaxMotorForce, MotorForceLerpAmount);
+                    _slimeMono.SetJointMotor(MaxMotorForce,lerp);
+                }
+                else
+                {
+                    var hinge = _slimeMono.Joint;
+                    var lerp = Mathf.Lerp(hinge.motor.force, 0, MotorForceLerpAmount);
+                    _slimeMono.SetJointMotor(MaxMotorForce,lerp);
+                }
+
+                yield return null;
+            }
+
+            yield return null;
         }
 
-        public void Execute()
-        {
-            
-        }
-
-        public void FixedExecute()
+        public void CleanUp()
         {
             
         }
---
+++ Assets/Features/Slime/SlimeMono.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Features.Slime
+{
+    public class SlimeMono : MonoBehaviour
+    {
+        public Rigidbody SlimeRB;
+        public Rigidbody HammerRB;
+        public HingeJoint Joint;
+
+        public Vector3 SlimeScale => SlimeRB.transform.localScale;
+        public Vector3 HammerScale => HammerRB.transform.localScale;
+
+        public void SetSlimeScale(Vector3 scale)
+        {
+            SlimeRB.transform.localScale = scale;
+        }
+        
+        public void SetHammerScale(Vector3 scale)
+        {
+            HammerRB.transform.localScale = scale;
+        }
+        
+        public void SetJointMotor(float targetVel, float force)
+        {
+            Joint.useMotor = true;
+            Joint.motor = new JointMotor
+            {
+                targetVelocity = targetVel,
+                force = force
+            };
+        }
+
+    }
+}
