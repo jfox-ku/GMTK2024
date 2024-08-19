adecfc2c3a37c80b67c837c3b6be9c2da1de7492
Yigitc <crkygt@gmail.com>
2024-08-19 15:33:17 +0300

    Noise1 move speed

--- Assets/Features/Slime/BlendShapeController.cs
+++ Assets/Features/Slime/BlendShapeController.cs
@@ -7,9 +7,15 @@ public class BlendShapeController : MonoBehaviour
 {
     [SerializeField] protected SkinnedMeshRenderer SkinnedMeshRenderer;
     public int BlendShapeIndex;
+    public int BlendShapeMoveIndex;
 
     public virtual void SetBlendShapeValue(float value)
     {
         SkinnedMeshRenderer.SetBlendShapeWeight(BlendShapeIndex, value);
+    }
+
+    public virtual void SetBlendShapeDirection(float value)
+    {
+        SkinnedMeshRenderer.SetBlendShapeWeight(BlendShapeMoveIndex,value);
     }
 }
--- Assets/Features/Slime/FloatBlendShapeController.cs
+++ Assets/Features/Slime/FloatBlendShapeController.cs
@@ -7,15 +7,24 @@ namespace Features.Slime
     {
         public FloatVariable FloatVariable;
         public AnimationCurve BlendShapeCurve;
+        public FloatVariable FloatVariableMoveSpeed;
+
+        [SerializeField] private float speed;
         
         private void Update()
         {
             SetBlendShapeValue(BlendShapeCurve.Evaluate(FloatVariable.Value));
+            SetBlendShapeDirection(BlendShapeCurve.Evaluate(FloatVariableMoveSpeed.Value));
         }
 
         public override void SetBlendShapeValue(float value)
         {
             SkinnedMeshRenderer.SetBlendShapeWeight(BlendShapeIndex, BlendShapeCurve.Evaluate(value));
+        }
+
+        public override void SetBlendShapeDirection(float value)
+        {
+            SkinnedMeshRenderer.SetBlendShapeWeight(BlendShapeMoveIndex,Mathf.Lerp(0, FloatVariableMoveSpeed, Mathf.PingPong(Time.time * speed, 1f)));
         }
     }
 }
