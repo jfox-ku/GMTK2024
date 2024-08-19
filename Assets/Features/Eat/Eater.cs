bcbdbaec9f0803da0bf3572224f82c2689b208ff
Atahan <atahanylmaz@gmail.com>
2024-08-19 23:13:20 +0300

    scene 0 setup

---
+++ Assets/Features/Eat/Eater.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Features.Eat
+{
+    public class Eater : MonoBehaviour
+    {
+        public float Size;
+        public Vector3 Center => transform.position;
+        private void OnCollisionEnter(Collision collision)
+        {
+            var edible = collision.gameObject.GetComponent<Edible>();
+            if (edible != null)
+            {
+                TryEat(edible);
+            }
+        }
+
+        private void TryEat(Edible edible)
+        {
+            if (edible.Size <= Size)
+            {
+                Size += edible.Size * 0.1f;
+                edible.EatenBy(this);
+            }  
+        }
+    }
+}
---
+++ Assets/Features/Eat/Edible.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Features.Eat
+{
+    public class Edible : MonoBehaviour
+    {
+        public float Size;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube(transform.position, Vector3.one * Size);
+        }
+
+        public void EatenBy(Eater eater)
+        {
+            var colliders = transform.GetComponentsInChildren<Collider>();
+            foreach (var c in colliders)
+            {
+                c.enabled = false;
+            }
+
+            transform.DOMove(eater.Center, 0.5f).OnComplete(() =>
+            {
+                Destroy(gameObject);
+            });
+        }
+    }
+}
